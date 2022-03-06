5305ed47a9d0759b0cd99958dabb6c4d99d8b6e3
Shadeling <mironow.slav@yandex.ru>
2022-03-06 12:06:34 +0300

    readme + different platforms

--- Assets/Scripts/World/LevelGen.cs
+++ Assets/Scripts/World/LevelGen.cs
@@ -11,9 +11,12 @@ public class LevelGen : MonoBehaviour
     [SerializeField] GameObject BigAxeTrap;
     [SerializeField] GameObject Coin;
 
-    private int PlaneTypes = 1;
+    private int PlaneTypes = 4;
 
-    
+    private void Awake()
+    {
+    }
+
     public GameObject GeneratePlane(Vector3 position)
     {
         //Plane plane = Instantiate(new Plane(), position, Quaternion.identity);
@@ -23,7 +26,9 @@ public class LevelGen : MonoBehaviour
         switch(Random.Range(0, PlaneTypes))
         {
             case 0: FillStandartPlane(plane); break;
-            case 1: FillMoneyPlane(plane); break;
+            case 1: FillStandartPlane(plane); break;
+            case 2: FillBigAxePlane(plane); break;
+            case 3: FillSmallAxePlane(plane); break;
         }
 
         return plane;
@@ -31,21 +36,64 @@ public class LevelGen : MonoBehaviour
 
     private void FillStandartPlane(GameObject plane)
     {
-        for(int i=0; i<2; i++)
+        int layerMask = 1 << 8;
+        Vector3 pos;
+
+
+        for (int i=0; i<2; i++)
         {
             var enemy = Instantiate(FireTrap, plane.transform);
-            //enemy.transform.localPosition = Vector3.zero;
-            enemy.transform.Translate(new Vector3(Random.Range(-4f, 5f), 0, Random.Range(-4f, 5f)));
+            int steps = 0;
+            //do
+            //{
+                pos = new Vector3(Random.Range(-3.5f, 3.5f), 0, Random.Range(-3.5f, 3.5f));
+                steps++;
+            //}
+            //while (!Physics.CheckSphere(enemy.transform.position+pos, 2) || steps>10);
+
+            Debug.Log(steps);
+            enemy.transform.Translate(pos);
         }
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < 2; i++)
         {
             var enemy = Instantiate(SpikeTrap, plane.transform);
-            //enemy.transform.localPosition = Vector3.zero;
-            enemy.transform.Translate(new Vector3(Random.Range(-4f, 5f), 0, Random.Range(-4f, 5f)));
+            int steps = 0;
+            //do
+            //{
+                pos = new Vector3(Random.Range(-3.5f, 3.5f), 0, Random.Range(-3.5f, 3.5f));
+                steps++;
+            //}
+            //while (!Physics.CheckSphere(pos, 1, layerMask) || steps > 10);
+
+            enemy.transform.Translate(pos);
         }
     }
 
     private void FillMoneyPlane(GameObject plane)
+    {
+
+    }
+
+    private void FillSmallAxePlane(GameObject plane)
+    {
+        var leftRight = Random.Range(0, 2) == 0 ? 1 : -1;
+
+        var enemy1 = Instantiate(SmallAxeTrap, plane.transform);
+        var enemy2 = Instantiate(SmallAxeTrap, plane.transform);
+
+        enemy1.transform.Translate(new Vector3(4 * leftRight, 0, 5 ));
+        enemy2.transform.Translate(new Vector3(-4 * leftRight, 0, -5));
+    }
+
+    private void FillBigAxePlane(GameObject plane)
+    {
+        var enemy = Instantiate(BigAxeTrap, plane.transform);
+        var leftRight = Random.Range(0, 2)==0 ? 1 : -1;
+
+        enemy.transform.Translate(new Vector3(3 * leftRight, 4.5f , Random.Range(-3.5f, 3.5f) ));
+    }
+
+    private void FillEmptyPlane(GameObject plane)
     {
 
     }
